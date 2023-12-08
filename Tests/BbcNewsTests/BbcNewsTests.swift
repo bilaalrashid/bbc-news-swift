e82588708a5796a620826a18b918a20e87c61f70
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-12-08 23:26:28 +0000

    Test API URL checking

--- Tests/BbcNewsTests/BbcNewsTests.swift
+++ Tests/BbcNewsTests/BbcNewsTests.swift
@@ -2,11 +2,18 @@ import XCTest
 @testable import BbcNews
 
 final class BbcNewsTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
+    func testIsApiUrl() throws {
+        let invalidUrlResult = BbcNews.isAPIUrl(url: "invalid")
+        XCTAssertFalse(invalidUrlResult, "An invalid URL does not match the API")
 
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        let differentHostResult = BbcNews.isAPIUrl(url: "https://bilaal.co.uk")
+        XCTAssertFalse(differentHostResult, "A URL with on a different hostname does not match the API")
+
+        let tlsResult = BbcNews.isAPIUrl(url: "https://news-app.api.bbc.co.uk/fd/abl?clientName=Chrysalis&page=cwgdx0ppwnzt&type=topic")
+        XCTAssertTrue(tlsResult, "A URL on the correct hostname using HTTPS, correctly matches the API")
+
+        // swiftlint:disable:next force_https
+        let insecureResult = BbcNews.isAPIUrl(url: "http://news-app.api.bbc.co.uk/fd/abl?clientName=Chrysalis&page=cwgdx0ppwnzt&type=topic")
+        XCTAssertTrue(insecureResult, "A URL on the correct hostname using HTTP, correctly matches the API")
     }
 }
