7b0efc6c9625df501b30674ac64da7c00535c144
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2023-12-10 17:49:23 +0000

    Update schema for FDPresentation

--- Sources/BbcNews/Models/Discovery/Link/FDPresentation.swift
+++ Sources/BbcNews/Models/Discovery/Link/FDPresentation.swift
@@ -11,10 +11,12 @@ public struct FDPresentation: Codable, Equatable, Hashable {
     public let type: String
     public let title: String?
     public let canShare: Bool?
+    public let contentSource: String?
 
-    public init(type: String, title: String? = nil, canShare: Bool? = nil) {
+    public init(type: String, title: String? = nil, canShare: Bool? = nil, contentSource: String? = nil) {
         self.type = type
         self.title = title
         self.canShare = canShare
+        self.contentSource = contentSource
     }
 }
---
+++ Tests/BbcNewsTests/Models/Link/FDPresentationTests.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Bilaal Rashid on 10/12/2023.
+//
+
+import Foundation
