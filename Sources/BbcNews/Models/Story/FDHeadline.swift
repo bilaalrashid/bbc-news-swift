f3c2ff7b80922e01367ca3363522e486e50c161f
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2024-05-04 18:44:48 +0100

    Update schema of FDHeadline

--- Sources/BbcNews/Models/Story/FDHeadline.swift
+++ Sources/BbcNews/Models/Story/FDHeadline.swift
@@ -10,16 +10,24 @@ import Foundation
 public struct FDHeadline: Codable, Equatable, Hashable {
     public let type: String
     public let text: String
-    public let lastUpdated: Int
+    public let lastUpdated: Int?
+    public let firstPublished: Int?
+    public let lastPublished: Int?
     public let byline: FDHeadlineByline?
     public let topic: FDTopic?
     public let languageCode: String
     public let readTimeMinutes: Int
 
+    public var published: Int? {
+        return self.lastUpdated ?? self.lastPublished ?? self.firstPublished
+    }
+
     public init(
         type: String,
         text: String,
-        lastUpdated: Int,
+        lastUpdated: Int? = nil,
+        firstPublished: Int? = nil,
+        lastPublished: Int? = nil,
         byline: FDHeadlineByline? = nil,
         topic: FDTopic? = nil,
         languageCode: String,
@@ -28,6 +36,8 @@ public struct FDHeadline: Codable, Equatable, Hashable {
         self.type = type
         self.text = text
         self.lastUpdated = lastUpdated
+        self.firstPublished = firstPublished
+        self.lastPublished = lastPublished
         self.byline = byline
         self.topic = topic
         self.languageCode = languageCode
