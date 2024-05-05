9dae39a33d4b9b0a638dcaca45ee7861e03ceb4d
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2024-05-05 19:03:59 +0100

    Don't include swiftlint as project dependency

--- Package.swift
+++ Package.swift
@@ -1,8 +1,6 @@
 // swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// Any changes to this file should be reflected in PackageTests.swift.
-
 import PackageDescription
 
 let package = Package(
@@ -22,21 +20,16 @@ let package = Package(
             targets: ["BbcNews"]
         )
     ],
-    dependencies: [
-        .package(url: "https://github.com/realm/SwiftLint", from: "0.54.0")
-    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "BbcNews",
-            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
+            name: "BbcNews"
         ),
         .testTarget(
             name: "BbcNewsTests",
             dependencies: ["BbcNews"],
-            resources: [.copy("Resources")],
-            plugins: [.plugin(name: "SwiftLintPlugin", package: "SwiftLint")]
+            resources: [.copy("Resources")]
         )
     ]
 )
