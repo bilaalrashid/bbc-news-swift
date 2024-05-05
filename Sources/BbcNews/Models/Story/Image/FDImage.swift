572120e89f5eef67aa184f15060e03d52b5b60d0
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2024-05-05 16:44:14 +0100

    Calculate URLs for FDImages

--- Sources/BbcNews/Models/Story/Image/FDImage.swift
+++ Sources/BbcNews/Models/Story/Image/FDImage.swift
@@ -17,4 +17,28 @@ public struct FDImage: Codable, Equatable, Hashable {
         self.source = source
         self.metadata = metadata
     }
+
+    /// Returns the URL that provides the image in the largest possible width, or `nil` if no image URLs can be found.
+    public var largestImageUrl: String? {
+        return self.largestImageUrl(upTo: .infinity)
+    }
+
+    /// Returns a URL that provides the image in the largest possible width within the specified maximum.
+    ///
+    /// - Parameter maxWidth: The maximum width which the URL should return an image for.
+    /// - Returns: A URL that returns a version of the image, or `nil` if no image URLs can be found.
+    public func largestImageUrl(upTo maxWidth: Double) -> String? {
+        if self.source.sizingMethod.type == "SPECIFIC_WIDTHS" {
+            let allowedWidths = self.source.sizingMethod.widths.filter { Double($0) <= maxWidth }
+
+            if let maxSize = allowedWidths.last {
+                return self.source.url.replacingOccurrences(of: self.source.sizingMethod.widthToken, with: String(maxSize))
+            }
+
+            return nil
+        }
+
+        // If we don't recognise the sizing method, assume the URL is not templated.
+        return self.source.url
+    }
 }
---
+++ Tests/BbcNewsTests/FDImageTests.swift
@@ -0,0 +1,64 @@
+//
+//  FDImageTests.swift
+//  BbcNewsTests
+//
+//  Created by Bilaal Rashid on 05/05/2024.
+//
+
+import XCTest
+@testable import BbcNews
+
+final class FDImageTests: XCTestCase {
+    func testImageUrls() throws {
+        let image = self.getFDImage(sizingMethod: "SPECIFIC_WIDTHS")
+
+        XCTAssertEqual(
+            image.largestImageUrl,
+            "https://example.invalid/img/300/example.jpg",
+            "Fails to return URL for the largest width"
+        )
+        XCTAssertEqual(
+            image.largestImageUrl(upTo: 250),
+            "https://example.invalid/img/200/example.jpg",
+            "Fails to return the largest width up to the specified maximum"
+        )
+        XCTAssertEqual(
+            image.largestImageUrl(upTo: 200),
+            "https://example.invalid/img/200/example.jpg",
+            "Fails to return the largest width when equal to the specified maximum"
+        )
+    }
+
+    func testInvalidSizingMethods() throws {
+        let image = self.getFDImage(sizingMethod: "INVALID")
+
+        XCTAssertEqual(
+            image.largestImageUrl,
+            "https://example.invalid/img/{width}/example.jpg",
+            "Fails to return an unmodified URL for an unrecognised sizing method"
+        )
+        XCTAssertEqual(
+            image.largestImageUrl(upTo: 250),
+            "https://example.invalid/img/{width}/example.jpg",
+            "Fails to return an unmodified URL for an unrecognised sizing method"
+        )
+    }
+
+    private func getFDImage(sizingMethod: String) -> FDImage {
+        return FDImage(
+            type: "Image",
+            source: FDImageSource(
+                url: "https://example.invalid/img/{width}/example.jpg",
+                sizingMethod: FDImageSizingMethod(
+                    type: sizingMethod,
+                    widthToken: "{width}",
+                    widths: [
+                        100,
+                        200,
+                        300
+                    ]
+                )
+            )
+        )
+    }
+}
