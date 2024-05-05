3e2bec28ef465d1d2389a3ccbd40887635de0c1d
Bilaal Rashid <42493384+bilaalrashid@users.noreply.github.com>
2024-05-05 19:05:01 +0100

    Check FDItem types don't decode as unknown in test suite

--- Tests/BbcNewsTests/SchemaTests.swift
+++ Tests/BbcNewsTests/SchemaTests.swift
@@ -9,10 +9,24 @@ import XCTest
 @testable import BbcNews
 
 final class SchemaTests: XCTestCase {
-    func testStory() throws {
-        XCTAssertNoThrow(try self.decode(FDResult.self, file: "IndexDiscovery"))
-        XCTAssertNoThrow(try self.decode(FDResult.self, file: "Topic"))
-        XCTAssertNoThrow(try self.decode(FDResult.self, file: "Story"))
+    func testFDResult() throws {
+        // Ignore call-to-action banner, which we don't support
+        try self.executeTest(file: "IndexDiscovery", unknownIndexesToIgnore: [41])
+        try self.executeTest(file: "Topic")
+        try self.executeTest(file: "Story")
+    }
+
+    private func executeTest(file: String, unknownIndexesToIgnore: [Int] = []) throws {
+        XCTAssertNoThrow(try self.decode(FDResult.self, file: file), "Failed to decode \(file) to FDResult")
+
+        let indexDiscovery = try self.decode(FDResult.self, file: file)
+        for (index, item) in indexDiscovery.data.items.enumerated() {
+            if unknownIndexesToIgnore.contains(index) {
+                continue
+            }
+
+            XCTAssertNotEqual(item, .unknown, "Failed to decode \(file) FDItem at index \(index) as non-unknown item")
+        }
     }
 
     private func decode<T: Decodable>(_ type: T.Type, file: String) throws -> T {
